768c4c3edd6acdd53a85203cfaa1a316b05b59ce
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2024-09-27 11:40:39 -0400

    summarize

---
+++ stats/03-summarize.R
@@ -0,0 +1,69 @@
+#!/usr/bin/env Rscript --vanilla
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Fri Sep 27 11:32:05 2024
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+suppressPackageStartupMessages(library(magrittr))
+library(ggplot2)
+library(patchwork)
+library(prismatic)
+library(paletteer)
+library(data.table)
+#library(rlang)
+library(GetoptLong)
+library(logger)
+
+# args --------------------------------------------------------------------
+
+# s: string, i: integer, f: float, !: boolean
+# @: array
+# %: hash
+# default: default value specified here.
+verbose <- FALSE
+spec <- "
+Usage: Rscript foorbar.R [options]
+Options:
+
+<verbose!> Print messages
+"
+
+GetoptLong.options(help_style = "two-column")
+GetoptLong(spec, template_control = list(opt_width = 21))
+
+# src ---------------------------------------------------------------------
+
+
+# header ------------------------------------------------------------------
+log_threshold(TRACE)
+log_layout(layout_glue_colors)
+
+# future::plan(future::multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+gseidlist <- c(
+  "GSE163668",
+  "GSE149689",
+  "GSE155223",
+  "GSE155673",
+  "GSE157344",
+  "GSE166992",
+  "GSE171555"
+)
+
+# body --------------------------------------------------------------------
+
+
+
+# footer ------------------------------------------------------------------
+
+# future::plan(future::sequential)
+
+# save image --------------------------------------------------------------
