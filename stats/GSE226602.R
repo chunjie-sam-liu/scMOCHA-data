5eeffd01f7c04de47753016ccae729f14ea4a85c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2024-12-08 20:12:23 -0500

    GSE226602

--- stats/GSE226602.R
+++ stats/GSE226602.R
@@ -140,11 +140,13 @@ metadata_clean
 
 # ggstats correlation plot ------------------------------------------------
 
+
+
 ggstatsplot::ggscatterstats(
-  data = metadata_clean |> dplyr::filter(Disease == "Healthy Control"),
+  data = metadata_clean,
   x = Age,
   y = `# of somatic variants`,
-  title = "Correlation between Age and Number of Somatic Variants",
+  title = "Age",
   xlab = "Age",
   ylab = "Number of Somatic Variants",
   ggtheme = ggplot2::theme_minimal()
@@ -152,12 +154,179 @@ ggstatsplot::ggscatterstats(
 p
 
 
-# ggsave(
-#   filename = file.path(outdir, "correlation_age_somatic_variants.png"),
-#   plot = p,
-#   width = 8,
-#   height = 6
-# )
+
+ggstatsplot::ggscatterstats(
+  data = metadata_clean |> dplyr::filter(Disease == "Healthy Control"),
+  x = Age,
+  y = `# of somatic variants`,
+  title = "Age",
+  xlab = "Age",
+  ylab = "Number of Somatic Variants",
+  ggtheme = ggplot2::theme_minimal()
+) -> p
+p
+
+# Age
+# # of cells after filtering
+anno_meta_info |>
+  dplyr::glimpse()
+
+anno_meta_info |>
+  dplyr::mutate(
+    avg_depth = purrr::map_dbl(
+      .x = depth,
+      .f = \(.d) {
+        mean(.d$depth)
+      }
+    )
+  ) |>
+  dplyr::mutate(
+    somatic_variant = purrr::map(
+      .x = somatic_variant,
+      .f = \(.x) {
+        .x$somatic
+      }
+    )
+  ) |>
+  dplyr::select(
+    srrid,
+    ncells = `number of cells after filtering`,
+    numi = `median UMI counts per cell`,
+    avg_depth,
+    nmut_somatic,
+    age,
+    gender,
+    disease,
+    haplo_violin,
+    somatic_variant
+  ) ->
+anno_meta_info_clean
+
+
+anno_meta_info_clean |>
+  ggstatsplot::ggscatterstats(
+    x = ncells,
+    y = nmut_somatic,
+    title = "Number of Cells",
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+  ) -> p_ncells
+p_ncells
+
+anno_meta_info_clean |>
+  ggstatsplot::ggscatterstats(
+    x = numi,
+    y = nmut_somatic,
+    title = "Number of UMI",
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+  ) -> p_numi
+p_numi
+
+anno_meta_info_clean |>
+  ggstatsplot::ggscatterstats(
+    x = avg_depth,
+    y = nmut_somatic,
+    title = "Average Depth",
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+  ) -> p_avg_depth
+p_avg_depth
+
+anno_meta_info_clean |>
+  ggstatsplot::ggscatterstats(
+    x = age,
+    y = nmut_somatic,
+    title = "Age",
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+  ) -> p_age
+p_age
+
+anno_meta_info_clean |> dplyr::glimpse()
+
+anno_meta_info_clean |>
+  ggstatsplot::ggbetweenstats(
+    x = gender,
+    y = nmut_somatic,
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+    title = "Gender",
+  ) -> p_gender
+p_gender
+
+anno_meta_info_clean |>
+  ggstatsplot::ggbetweenstats(
+    x = disease,
+    y = nmut_somatic,
+    xlab = "",
+    ylab = "Number of Somatic Variants",
+    title = "Disease"
+  ) -> p_disease
+p_disease
+
+wrap_plots(list(p_ncells, p_numi, p_avg_depth, p_age, p_gender, p_disease), ncol = 3) -> p_combined
+p_combined
+
+outdir_plot <- file.path(outdir, "plot")
+dir.create(outdir_plot, showWarnings = FALSE, recursive = TRUE)
+ggsave(
+  path = outdir_plot,
+  filename = "correlation_somatic_variants.pdf",
+  plot = p_combined,
+  width = 20,
+  height = 10
+)
+
+
+anno_meta_info_clean |>
+  ggstatsplot::ggscatterstats(
+    x = avg_depth,
+    y = ncells
+  )
+
+
+anno_meta_info_clean |>
+  dplyr::mutate(
+    cell_variant = purrr::map2(
+      .x = haplo_violin,
+      .y = somatic_variant,
+      .f = \(.x, .y) {
+        .x |>
+          dplyr::filter(
+            variant %in% .y
+          )
+      }
+    )
+  ) ->
+anno_meta_info_clean_cell_variant
+
+
+anno_meta_info_clean_cell_variant |>
+  dplyr::select(-haplo_violin, -somatic_variant) |>
+  tidyr::unnest(cols = cell_variant) ->
+anno_meta_info_clean_cell_variant_unnest
+
+anno_meta_info_clean_cell_variant_unnest |> dplyr::glimpse()
+
+
+anno_meta_info_clean_cell_variant_unnest |>
+  dplyr::filter(variant == "3176A>T", cluster == "B") |>
+  dplyr::mutate(
+    depth = exp(depth)
+  ) |>
+  dplyr::mutate(
+    depth = ifelse(depth > 500, 500, depth)
+  ) |>
+  ggstatsplot::ggscatterstats(
+    x = depth,
+    y = af
+  )
+
+
+
+
+
 # footer ------------------------------------------------------------------
 
 # future: :plan(future: :sequential)
