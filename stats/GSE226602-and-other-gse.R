7aff2d789c3f912a789826bdefd7a0d180fd2f4c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2024-12-15 15:26:27 -0500

    add initial R script for data analysis and visualization

---
+++ stats/GSE226602-and-other-gse.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript --vanilla
+# Metainfo ----------------------------------------------------------------
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: `r date()`
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+suppressPackageStartupMessages(library(magrittr))
+library(ggplot2)
+library(patchwork)
+library(prismatic)
+library(paletteer)
+library(data.table)
+# library(rlang)
+library(GetoptLong)
+library(logger)
+
+# args --------------------------------------------------------------------
+
+# s: string, i: integer, f: float, !: boolean
+# @: array
+# %: hash
+# default: default value specified here.
+verbose <- FALSE
+spec <- "
+Usage: Rscript foorbar.R [options]
+Options:
+
+<verbose!> Print messages
+"
+
+GetoptLong.options(help_style = "two-column")
+GetoptLong(spec, template_control = list(opt_width = 21))
+
+# src ---------------------------------------------------------------------
+
+# header ------------------------------------------------------------------
+log_threshold(TRACE)
+log_layout(layout_glue_colors)
+
+# future: :plan(future: :multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+
+# body --------------------------------------------------------------------
+
+
+# footer ------------------------------------------------------------------
+
+# future: :plan(future: :sequential)
+
+# save image --------------------------------------------------------------
