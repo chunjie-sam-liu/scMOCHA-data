4edead969c29586ab19142e45b550fa597284a67
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2024-12-19 15:53:32 -0500

    add script to automate git pull for project repositories

---
+++ stats/gitpull.dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2024-12-19 14:39:07
+# @DESCRIPTION:
+# @VERSION: v0.0.1
+cd /home/liuc9/large-scale/liuc9/scMOCHA
+git pull
+cd /home/liuc9/large-scale/liuc9/scMOCHA-data
+git pull
