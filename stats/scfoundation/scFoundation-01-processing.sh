9d92955b5e634ad1a9e61b5d8d6325a60c180688
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-01-02 15:58:03 -0500

    add scFoundation processing script and enhance sratable with cell type analysis

---
+++ stats/scfoundation/scFoundation-01-processing.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2025-01-02 15:14:02
+# @DESCRIPTION:
+# @VERSION: v0.0.1
+
+gses=(
+  "GSE140881"
+  "GSE142595"
+  "GSE149313"
+  "GSE154386"
+  "GSE159117"
+  "GSE162117"
+  "GSE167825"
+  "GSE179566"
+  "GSE188632"
+  "GSE192391"
+)
+
+done=(
+  "GSE140881"
+  "GSE142595"
+  "GSE149313"
+  "GSE154386"
+  "GSE159117"
+  "GSE162117"
+  "GSE167825"
+  "GSE179566"
+  "GSE188632"
+  "GSE192391"
+)
+
+basedir="/home/liuc9/github/scMOCHA-data/data/scfoundation"
+
+# /home/liuc9/github/scMOCHA-data/src/01-sra-metadata.R
+sra_metadata() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/01-sra-metadata.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/01-sra-metadata.R -g ${gse} -b ${basedir} &
+  done
+}
+# sra_metadata
+
+# /home/liuc9/github/scMOCHA-data/src/02-sra-download-dump.R
+sra_download_dump() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/02-sra-download-dump.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/02-sra-download-dump.R -g ${gse} -b ${basedir} &
+  done
+}
+# sra_download_dump
+
+# /home/liuc9/github/scMOCHA-data/data/scfoundation/GSE140881/00.${gseid}.prefetch.sh
+prefetch() {
+  for gse in "${gses[@]}"; do
+    echo "bash /home/liuc9/github/scMOCHA-data/data/scfoundation/${gse}/00.${gse}.prefetch.sh"
+    cd /home/liuc9/github/scMOCHA-data/data/scfoundation/${gse}
+    bash 00.${gse}.prefetch.sh
+  done
+}
+prefetch
+
+# /home/liuc9/github/scMOCHA-data/data/scfoundation/GSE140881/01.${gseid}.prefetch.check.sh
+prefetch_check() {
+  for gse in "${gses[@]}"; do
+    echo "bash /home/liuc9/github/scMOCHA-data/data/scfoundation/${gse}/01.${gse}.prefetch.check.sh"
+    cd /home/liuc9/github/scMOCHA-data/data/scfoundation/${gse}
+    bash 01.${gse}.prefetch.check.sh
+  done
+}
+# prefetch_check
+
+# /home/liuc9/github/scMOCHA-data/src/03-sra-rename-gsm-merge.R
+sra_rename_gsm_merge() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/03-sra-rename-gsm-merge.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/03-sra-rename-gsm-merge.R -g ${gse} -b ${basedir} &
+  done
+}
+# sra_rename_gsm_merge
+
+# /home/liuc9/github/scMOCHA-data/src/04-scmocha-conf.R
+scmocha_conf() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/04-scmocha-conf.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/04-scmocha-conf.R -g ${gse} -b ${basedir} &
+  done
+}
+# scmocha_conf
+
+# run 04.{gse}.batch.sh
+scmocha_batch_run() {
+  for gse in "${gses[@]}"; do
+    echo "bash /home/liuc9/github/scMOCHA-data/data/04.${gse}.batch.sh"
+    cd /home/liuc9/github/scMOCHA-data/data/${gse}
+    bash 04.${gse}.batch.sh
+  done
+}
+# scmocha_batch_run
+
+# /home/liuc9/github/scMOCHA-data/src/05-parse-log.R
+parse_log() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/05-parse-log.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/05-parse-log.R -g ${gse} -b ${basedir} &
+  done
+}
+# parse_log
+
+# /home/liuc9/github/scMOCHA-data/data/GSE279945/05.{gseid}.scmocha.cptargz.sh
+
+cptargz() {
+  for gse in "${gses[@]}"; do
+    echo "bash /home/liuc9/github/scMOCHA-data/data/${gse}/05.${gse}.scmocha.cptargz.sh"
+    cd /home/liuc9/github/scMOCHA-data/data/${gse}
+    bash 05.${gse}.scmocha.cptargz.sh
+  done
+}
+# cptargz
+
+# /home/liuc9/github/scMOCHA-data/data/GSE279945/07.GSE279945.scmocha.untargz.sh
+untargz() {
+  for gse in "${gses[@]}"; do
+    echo "bash /home/liuc9/github/scMOCHA-data/data/${gse}/07.${gse}.scmocha.untargz.sh"
+    cd /home/liuc9/github/scMOCHA-data/data/${gse}
+    bash 07.${gse}.scmocha.untargz.sh
+  done
+}
+# untargz
+
+# wait
+# /home/liuc9/github/scMOCHA-data/src/06-collect-variants.R
+collect_variants() {
+  for gse in "${gses[@]}"; do
+    echo "Rscript /home/liuc9/github/scMOCHA-data/src/06-collect-variants.R -g ${gse} -b ${basedir}"
+    Rscript /home/liuc9/github/scMOCHA-data/src/06-collect-variants.R -g ${gse} -b ${basedir} &
+  done
+}
+# collect_variants
--- stats/scfoundation/sratable.R
+++ stats/scfoundation/sratable.R
@@ -46,6 +46,59 @@ log_layout(layout_glue_colors)
 # future: :plan(future: :multisession, workers = 10)
 
 # function ----------------------------------------------------------------
+fn_celltype_pie_plot <- function(.xxx_celltype) {
+  # pcc <- readr::read_tsv(file = "https://raw.githubusercontent.com/chunjie-sam-liu/chunjie-sam-liu.life/master/public/data/pcc.tsv")
+
+  .xxx_celltype |>
+    dplyr::ungroup() %>%
+    dplyr::mutate(csum = rev(cumsum(rev(n)))) %>%
+    dplyr::mutate(pos = n / 2 + dplyr::lead(csum, 1)) %>%
+    dplyr::mutate(pos = dplyr::if_else(is.na(pos), n / 2, pos)) %>%
+    dplyr::mutate(percentage = n / sum(n)) %>%
+    dplyr::arrange(-n) |>
+    dplyr::mutate(
+      celltype = as.factor(celltype),
+      levels = celltype
+    ) |>
+    ggplot(aes(x = "", y = n, fill = celltype)) +
+    geom_bar(
+      stat = "identity",
+      width = 1,
+      color = "white",
+      show.legend = FALSE
+    )
+  # scale_fill_brewer(palette = "Dark2", name = NULL) +
+  # scale_fill_manual(
+  #   name = NULL,
+  #   values = pcc$color,
+  # ) +
+  # scale_color_manual(
+  #   name = NULL,
+  #   values = pcc$color
+  # ) +
+  ggrepel::geom_label_repel(
+    aes(
+      y = pos,
+      # label = glue::glue("{celltype}\n{n} ({scales::percent(percentage)})"),
+      fill = celltype,
+      # color = celltype
+    ),
+    size = 6,
+    # fill = "white",
+    nudge_x = 1,
+    show.legend = FALSE,
+  ) +
+    coord_polar(theta = "y", start = 0) +
+    theme_void() +
+    theme(
+      plot.title = element_text(
+        # vjust = -2,
+        hjust = 0.5,
+        size = 22,
+      ),
+      legend.position = "none"
+    )
+}
 
 
 # load data ---------------------------------------------------------------
@@ -130,6 +183,9 @@ sra_df |>
   dplyr::relocate(
     run_accession,
     .before = 1
+  ) |>
+  dplyr::mutate(
+    source_name = stringr::str_trim(source_name)
   ) ->
 cleaned_sample_df_sra
 
@@ -142,6 +198,131 @@ data.table::fwrite(
   )
 )
 
+cleaned_sample_df_sra |>
+  dplyr::filter(library_strategy == "RNA-Seq") ->
+cleaned_sample_df_sra_rnaseq
+
+pbmc <- c("PBMCs", "PBMC", "Cryopreserved PBMC", "pbmc", "engineered PBMCs")
+
+cleaned_sample_df_sra_rnaseq |>
+  dplyr::filter(!is.na(source_name)) |>
+  dplyr::select(experiment_name, source_name) |>
+  dplyr::distinct() |>
+  # dplyr::filter(grepl("pbmc", source_name, ignore.case = T)) |>
+  dplyr::mutate(
+    source_name = ifelse(
+      source_name %in% pbmc,
+      "PBMC",
+      source_name
+    )
+  ) |>
+  dplyr::count(source_name) |>
+  dplyr::mutate(
+    source_name = ifelse(n < 40, "others", source_name)
+  ) |>
+  dplyr::group_by(source_name) |>
+  dplyr::summarise(n = sum(n)) |>
+  dplyr::rename(celltype = source_name) |>
+  dplyr::arrange(-n) ->
+celltype_df
+celltype_df |>
+  data.table::fwrite(
+    file = file.path(
+      basedir,
+      "1.all.metadata.celltypes.csv" |> glue::glue()
+    )
+  )
+
+
+cleaned_sample_df_sra_rnaseq |>
+  dplyr::filter(!is.na(source_name)) |>
+  dplyr::filter(source_name %in% pbmc) |>
+  dplyr::glimpse()
+
+cleaned_sample_df_sra_rnaseq |>
+  dplyr::filter(!is.na(source_name)) |>
+  dplyr::filter(source_name %in% pbmc) |>
+  dplyr::mutate(
+    Run = run_accession,
+    `Sample Name` = experiment_name,
+    Experiment = experiment_accession,
+    study_name = study_name
+  ) |>
+  dplyr::relocate(
+    Run,
+    `Sample Name`,
+    Experiment,
+    study_name,
+    source_name,
+    .before = 1
+  ) ->
+sratable_pbmc
+
+already_processed <- c(
+  "GSE149689", "GSE155223", "GSE155673", "GSE157344", "GSE163668", "GSE166992", "GSE171555", "GSE181279", "GSE226602", "GSE161354",
+  "GSE175524",
+  "GSE206283",
+  "GSE226598",
+  "GSE235050",
+  "GSE261140",
+  "GSE279945"
+)
+
+sratable_pbmc |>
+  data.table::fwrite(
+    file = file.path(
+      basedir,
+      "1.all.metadata.celltypes.pbmc.csv" |> glue::glue()
+    )
+  )
+
+
+sratable_pbmc |>
+  dplyr::filter(!study_name %in% already_processed) |>
+  dplyr::group_by(study_name) |>
+  dplyr::pull(study_name) |>
+  unique() ->
+new_gseids_pbmc
+
+
+new_gseids_pbmc <- c("GSE140881", "GSE142595", "GSE149313", "GSE154386", "GSE159117", "GSE162117", "GSE167825", "GSE179566", "GSE188632", "GSE192391")
+
+readr::write_lines(
+  x = new_gseids_pbmc,
+  file = file.path(
+    basedir,
+    "1.new_gseids_pbmc.txt" |> glue::glue()
+  )
+)
+
+sratable_pbmc |>
+  dplyr::filter(study_name %in% new_gseids_pbmc) |>
+  dplyr::mutate(gseid = study_name) |>
+  dplyr::group_by(gseid) |>
+  tidyr::nest() |>
+  dplyr::ungroup() |>
+  dplyr::mutate(
+    a = purrr::map2(
+      .x = data,
+      .y = gseid,
+      .f = \(.x, .y) {
+        data.table::fwrite(
+          x = .x,
+          file = file.path(
+            basedir,
+            .y,
+            glue::glue("{.y}.SraRunTable")
+          )
+        )
+      }
+    )
+  )
+
+
+
+
+
+# run_accession,experiment_name,experiment_accession
 
 #
 # footer ------------------------------------------------------------------
