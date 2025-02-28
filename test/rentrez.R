f09315329bcfaa07a44794a5b271c963a0c9191f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-02-28 12:37:36 -0500

    refactor rentrez.R for consistency and add functionality to retrieve SRA project information

--- test/rentrez.R
+++ test/rentrez.R
@@ -2,17 +2,17 @@ library(rentrez)
 
 
 hox_paper <- entrez_search(
-  db="pubmed",
-  term="10.1016/j.neuron.2024.01.013[doi]"
+  db = "pubmed",
+  term = "10.1016/j.neuron.2024.01.013[doi]"
 )
 
 hox_paper$ids
-hox_data <- entrez_link(db="all", id=hox_paper$ids, dbfrom = "pubmed")
+hox_data <- entrez_link(db = "all", id = hox_paper$ids, dbfrom = "pubmed")
 hox_data$links$pubmed_sra
 
 hox_sra <- entrez_fetch(
-  db="sra",
-  id=hox_data$links$pubmed_sra,
+  db = "sra",
+  id = hox_data$links$pubmed_sra,
   rettype = "runinfo",
   retmode = "csv"
 )
@@ -37,31 +37,31 @@ entrez_search(
 )
 
 
-geo_search <- entrez_search(db="gds", term = gse_id)
+geo_search <- entrez_search(db = "gds", term = gse_id)
 geo_search$ids
-geo_summary <- entrez_summary(db="gds", id=geo_search$ids)
+geo_summary <- entrez_summary(db = "gds", id = geo_search$ids)
 geo_data <- entrez_fetch(
-  db="gds",
-  id=geo_search$ids,
-  rettype ="tsv"
+  db = "gds",
+  id = geo_search$ids,
+  rettype = "tsv"
 )
 
 
 cat(geo_data)
 
 
-# install.packages("rentrez")
+install.packages("rentrez")
 library(rentrez)
 
 
 gse_id <- "GSE226602"
-geo_search <- entrez_search(db="gds", term=gse_id)
+geo_search <- entrez_search(db = "gds", term = gse_id)
 
-geo_summary <- entrez_summary(db="gds", id=geo_search$ids)
+geo_summary <- entrez_summary(db = "sra", id = geo_search$ids)
 
 
 
-geo_data <- entrez_fetch(db="gds", id=geo_search$ids, rettype="xml")
+geo_data <- entrez_fetch(db = "gds", id = geo_search$ids, rettype = "xml")
 
 # Load XML package to parse the data
 library(XML)
@@ -70,9 +70,31 @@ geo_xml <- xmlParse(geo_data)
 # Extract Study Title and BioProject ID
 study_title <- xpathSApply(geo_xml, "//Title", xmlValue)
 bioproject_id <- xpathSApply(geo_xml, "//Bioproject//Accession", xmlValue)
+library(rentrez)
 
+# 获取项目信息
+project_id <- "PRJNA616380"
+search_res <- entrez_search(db = "sra", term = project_id)
+summary_res <- entrez_summary(db = "sra", id = search_res$ids)
 
+# 获取相关的 Run 信息
+sra_xml <- entrez_fetch(db = "sra", id = search_res$ids, rettype = "xml", parsed = TRUE)
 
+# 打印摘要
+summary_res
+# 查看 summary_res 的结构
+str(summary_res)
 
+# 提取某些关键字段
+project_title <- summary_res$title
+study_accession <- summary_res$study_acc
+submission_date <- summary_res$submission_date
+organism <- summary_res$organism
+experiments <- summary_res$experiment
 
-
+# 打印提取的信息
+print(project_title)
+print(study_accession)
+print(submission_date)
+print(organism)
+print(experiments)
