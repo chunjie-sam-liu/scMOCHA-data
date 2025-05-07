8068bf69dedd39eceea6bd840b82036b86e48cce
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-05-07 16:00:54 -0400

    feat: add color configuration for disease, chemistry, gender, and cell types in metacolors.json and 00-colors.R

---
+++ stats/stats/00-colors.R
@@ -0,0 +1,49 @@
+disease_colors <- c(
+  "Alzheimer's Disease" = "#BC3C29FF",
+  "COVID-19" = "#0072B5FF",
+  "Healthy" = "#E18727FF",
+  "Unknown" = "grey50",
+  "Other" = "grey"
+)
+
+chemistry_colors <- c(
+  "SC5P-PE" = "#440154FF",
+  "SC5P-R2" = "#31688EFF",
+  "SC3Pv3" = "#35B779FF",
+  "SC3Pv2" = "#FDE725FF"
+)
+
+gender_colors <- c(
+  "Female" = "#FC8D62FF",
+  "Male" = "#66C2A5FF",
+  "Unknown" = "grey50"
+)
+
+celltype_colors <- c(
+  "B" = "#66C2A5FF",
+  "CD4 T" = "#FC8D62FF",
+  "CD8 T" = "#8DA0CBFF",
+  "DC" = "#E78AC3FF",
+  "Mono" = "#A6D854FF",
+  "NK" = "#FFD92FFF",
+  "other" = "#B3B3B3FF",
+  "other T" = "#E5C494FF"
+)
+
+circos_track_colors <- c(
+  "phastCons100way" = "#FFD700",
+  "gnomad" = "#0000FF",
+  "homoplasmic_paf" = "#03DC62",
+  "homoplasmic_paf" = "#7a0202",
+  "homoplasmic_af" = "#a6fccb",
+  "homoplasmic_af" = "#FF0000",
+  "gene_name_bg" = "#EAF7FFFF"
+)
+
+mtdna_gene_type_colors <- c(
+  "D-Loop" = "#FFFFB3",
+  "MT tRNA" = "#FB8072",
+  "MT rRNA" = "#8DD3C7",
+  "Protein coding" = "#BEBADA",
+  "MT OLR" = "#FDB462"
+)
