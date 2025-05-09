a6764d28a9df4d596e699bc6a9d7066b46f13426
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-05-09 11:57:52 -0400

    fix: include chemistry in gse_data selection for improved coverage analysis

--- stats/stats/00-coverage.R
+++ stats/stats/00-coverage.R
@@ -4,8 +4,11 @@ gse_data <- readr::read_rds(
 
 
 gse_data |>
-  dplyr::select(gseid, srrid, depth_read) |>
-  tidyr::unnest(cols = depth_read) |>
+  dplyr::select(gseid, srrid, chemistry, depth_read) |>
+  tidyr::unnest(cols = depth_read) ->
+gse_data_depth
+
+gse_data_depth |>
   dplyr::group_by(pos) |>
   dplyr::summarise(
     depth = mean(depth, na.rm = T),
@@ -17,6 +20,23 @@ gse_data_coverage |>
   data.table::fwrite(
     file = file.path(
       "/home/liuc9/github/scMOCHA-data/stats/stats/zzz/clean-data/", "gse_data_coverage.csv"
+    ),
+    row.names = F
+  )
+
+
+gse_data_depth |>
+  dplyr::group_by(chemistry, pos) |>
+  dplyr::summarise(
+    depth = mean(depth, na.rm = T),
+  ) |>
+  dplyr::ungroup() ->
+gse_data_coverage_chemistry
+
+gse_data_coverage_chemistry |>
+  data.table::fwrite(
+    file = file.path(
+      "/home/liuc9/github/scMOCHA-data/stats/stats/zzz/clean-data/", "gse_data_coverage_chemistry.csv"
     ),
     row.names = F
   )
