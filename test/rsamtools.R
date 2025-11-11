60a6f7a1ad6e44fe16dab8e3da48ae8c09ea8b9c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-11-11 10:10:23 -0500

    feat: add bcell_bamfile path for processing barcoded BAM files

--- test/rsamtools.R
+++ test/rsamtools.R
@@ -18,3 +18,6 @@ rg2 <- readr::read_lines(
 )
 rgg <- setdiff(rg1, rg2)
 head(rgg)
+
+
+bcell_bamfile <- "/mnt/isilon/u01_project/large-scale/ting/raw/GSE235050/cromwell-executions/scMOCHABatch/66a50c5e-b7f0-4d1c-9c2f-9dec97c3a7f7/call-scMOCHA/shard-0/sub.scMOCHA/bf8e6c39-e9e6-4a4c-828e-af5935932eaf/call-call_mt_variants/execution/cluster/temp/temp_bam/B.temp1.barcoded.qc.bam"
