54a038705e1cd6d7022c7a604f2ba23ff153b52e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-11-26 15:54:18 -0500

    feat: update baser.R to include grid graphics and enhance plotting capabilities

--- test/baser.R
+++ test/baser.R
@@ -1,32 +1,57 @@
-my_data <- mtcars
-# Print the first 6 rows
-head(my_data, 6)
-plot(
-  x = my_data$wt, y = my_data$mpg,
-  pch = 16, frame = FALSE,
-  xlab = "wt", ylab = "mpg", col = "#2E9FDF"
-)
-plot(mtcars$wt, mtcars$mpg,
-  main = "Scatterplot in Base R",
-  xlab = "Car Weight", ylab = "MPG",
-  pch = 4, col = "blue", lwd = 10, cex = 1
-)
+#!/usr/bin/env Rscript
+# Metainfo ----------------------------------------------------------------
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2025-11-26 15:53:16
+# @DESCRIPTION: filename
+# @VERSION: v0.0.1
 
+# Library -----------------------------------------------------------------
 
-library(grid)
-text_grob <- textGrob("Hello", x = 0.5, y = 0.5)
-grid.draw(text_grob) # 显示它
-grid.rect(x = unit(0.5, "npc"), y = unit(1, "lines"), width = unit(3, "cm"))
-
-
-library(grid)
-
-# 创建 grob
-my_grob <- grobTree(
-  rectGrob(gp = gpar(fill = "lightblue")),
-  textGrob("Hello Grid", x = 0.5, y = 0.5, gp = gpar(fontsize = 20))
+load_pkg(
+  ggplot2,
+  patchwork,
+  prismatic,
+  paletteer,
+  data.table,
+  glue,
+  parallel,
+  GetoptLong,
+  scales,
+  fs,
+  jutils,
+  logger
 )
 
-# 绘图到窗口
-grid.newpage()
-grid.draw(my_grob)
+# args --------------------------------------------------------------------
+
+# s: string, i: integer, f: float, !: boolean
+# @: array
+# %: hash
+# default: default value specified here.
+verbose <- FALSE
+spec <- "
+Usage: Rscript foorbar.R [options]
+Options:
+
+<verbose!> Print messages
+"
+
+GetoptLong.options(help_style = "two-column")
+GetoptLong(spec, template_control = list(opt_width = 21))
+
+# header ------------------------------------------------------------------
+
+# load data ---------------------------------------------------------------
+
+# load conn ---------------------------------------------------------------
+
+# src ---------------------------------------------------------------------
+
+# function ----------------------------------------------------------------
+
+# body --------------------------------------------------------------------
+
+# footer ------------------------------------------------------------------
+
+# save image --------------------------------------------------------------
