05f7358f1484432d8af42c8c88b83c9fecc98c06
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-12-16 01:27:45 -0500

    feat: enhance circos plot generation with additional variant counting and multiple color schemes

--- analysis/high-res/06-circos.R
+++ analysis/high-res/06-circos.R
@@ -66,6 +66,11 @@ allvariants <- import(
   tidyr::unnest(
     cols = coord
   )
+
+allvariants |>
+  dplyr::select(variant_type, variant) |>
+  dplyr::distinct() |>
+  dplyr::count(variant_type)
 # load conn ---------------------------------------------------------------
 
 # src ---------------------------------------------------------------------
@@ -93,7 +98,7 @@ fn_plot_circos <- function(
   gap.degree = 1
   LENGTH <- 16569
 
-  library(circlize)
+  load_pkg(circlize)
   # ! init --------------------------------------------------------------------
 
   circos.clear()
@@ -117,6 +122,7 @@ fn_plot_circos <- function(
     axis.labels.cex = 0.8 * par("cex"),
     major.by = 1000
   )
+
   # ! track: phastCons100way --------------------------------------------------------------------
   phastCons100way <- import(
     "/home/liuc9/github/scMOCHA-data/config/chrM.phastCons100way.wigFix.qs"
@@ -270,25 +276,25 @@ fn_plot_circos <- function(
     dplyr::mutate(paf = n / N_INDIVIDUALS) |>
     dplyr::mutate(af = pmax(0, pmin(1, af, na.rm = TRUE))) -> forplot_somatic
 
-  circos.genomicTrack(
-    forplot_somatic,
-    track.height = 0.1,
-    ylim = c(0, 1),
-    track.margin = c(0, 0.01),
-    cell.padding = c(0, 0, 0, 0),
-    bg.border = NA,
-    bg.col = NA,
-    panel.fun = function(region, value, ...) {
-      pos = region$start
-      val = value$paf
-      circos.barplot(
-        value = val,
-        pos = pos,
-        col = color_circos_track["somatic_paf"],
-        border = color_circos_track["somatic_paf"]
-      )
-    }
-  )
+  # circos.genomicTrack(
+  #   forplot_somatic,
+  #   track.height = 0.1,
+  #   ylim = c(0, 1),
+  #   track.margin = c(0, 0.01),
+  #   cell.padding = c(0, 0, 0, 0),
+  #   bg.border = NA,
+  #   bg.col = NA,
+  #   panel.fun = function(region, value, ...) {
+  #     pos = region$start
+  #     val = value$paf
+  #     circos.barplot(
+  #       value = val,
+  #       pos = pos,
+  #       col = color_circos_track["somatic_paf"],
+  #       border = color_circos_track["somatic_paf"]
+  #     )
+  #   }
+  # )
 
   # ! gene name--------------------------------------------------------------------
   # ! gene region --------------------------------------------------------------------
@@ -345,7 +351,7 @@ fn_plot_circos <- function(
     forplot_homo,
     track.height = 0.1,
     ylim = c(0, 1),
-    track.margin = c(0, 0.01),
+    track.margin = c(0, 0.02),
     cell.padding = c(0, 0, 0, 0),
     bg.border = NA,
     bg.col = NA,
@@ -354,7 +360,7 @@ fn_plot_circos <- function(
       val = value$af
       circos.genomicPoints(
         region = region,
-        value = value,
+        value = val,
         pch = 2,
         cex = 0.5,
         col = color_circos_track["homo_af"],
@@ -371,7 +377,7 @@ fn_plot_circos <- function(
     ylim = c(0, 1),
     track.margin = c(0, 0.01),
     cell.padding = c(0, 0, 0, 0),
-    bg.border = NA,
+    bg.border = color_circos_track["hete_af"],
     bg.col = NA,
     panel.fun = function(region, value, ...) {
       pos = region$start
@@ -384,7 +390,7 @@ fn_plot_circos <- function(
       # )
       circos.genomicPoints(
         region = region,
-        value = value,
+        value = val,
         pch = 3,
         cex = 0.5,
         col = color_circos_track["hete_af"],
@@ -414,7 +420,7 @@ fn_plot_circos <- function(
       # )
       circos.genomicPoints(
         region = region,
-        value = value,
+        value = val,
         pch = 3,
         cex = 0.5,
         col = color_circos_track["somatic_af"],
@@ -428,6 +434,211 @@ fn_plot_circos <- function(
 
 # body --------------------------------------------------------------------
 
+{
+  outdir <- "/home/liuc9/github/scMOCHA-data/analysis/high-res/MANUSCRIPTFIGURES/"
+  pdf(
+    file = path(
+      outdir,
+      "CIRCOS-ALL-VARIANTS-HOMO1266-HETE703-COLOR-SELECT.pdf"
+    ),
+    width = 13,
+    height = 10
+  )
+
+  # Original color scheme
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FFD700",
+      "gnomad" = "#0000FF",
+      "homo_paf" = "#03DC62",
+      "hete_paf" = "#7A0202",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#A6FCCB",
+      "hete_af" = "#FF0000",
+      "somatic_af" = "#040404",
+      "gene_name_bg" = "#EAF7FF",
+      "coverage" = "#3FB5FF"
+    )
+  )
+
+  # Color Scheme 1: Tol Bright (color-blind safe)
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#DDAA33",
+      "gnomad" = "#004488",
+      "homo_paf" = "#44AA99",
+      "hete_paf" = "#BB5566",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#77AADD",
+      "hete_af" = "#EE6677",
+      "somatic_af" = "#333333",
+      "gene_name_bg" = "#F0F0F0",
+      "coverage" = "#228833"
+    )
+  )
+
+  # Color Scheme 2: Nature-inspired earth tones
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#E69F00",
+      "gnomad" = "#0072B2",
+      "homo_paf" = "#009E73",
+      "hete_paf" = "#D55E00",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#56B4E9",
+      "hete_af" = "#CC79A7",
+      "somatic_af" = "#2D2D2D",
+      "gene_name_bg" = "#EFEFEF",
+      "coverage" = "#F0E442"
+    )
+  )
+
+  # Color Scheme 3: Okabe-Ito palette (universally accessible)
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#F0E442",
+      "gnomad" = "#0173B2",
+      "homo_paf" = "#029E73",
+      "hete_paf" = "#DE8F05",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#56B4E9",
+      "hete_af" = "#CC78BC",
+      "somatic_af" = "#404040",
+      "gene_name_bg" = "#ECE7F2",
+      "coverage" = "#FBAFE4"
+    )
+  )
+
+  # Color Scheme 4: Vibrant but accessible
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FDB863",
+      "gnomad" = "#5E3C99",
+      "homo_paf" = "#1B7837",
+      "hete_paf" = "#D6604D",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#B2ABD2",
+      "hete_af" = "#F4A582",
+      "somatic_af" = "#1A1A1A",
+      "gene_name_bg" = "#F7F7F7",
+      "coverage" = "#66C2A5"
+    )
+  )
+
+  # Color Scheme 5: Cool professional tones
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FFD92F",
+      "gnomad" = "#4575B4",
+      "homo_paf" = "#74ADD1",
+      "hete_paf" = "#D73027",
+      "somatic_paf" = "#313695",
+      "homo_af" = "#ABD9E9",
+      "hete_af" = "#F46D43",
+      "somatic_af" = "#252525",
+      "gene_name_bg" = "#E0F3F8",
+      "coverage" = "#A50026"
+    )
+  )
+
+  # Color Scheme 6: Warm accent palette
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FEE08B",
+      "gnomad" = "#998EC3",
+      "homo_paf" = "#01665E",
+      "hete_paf" = "#BF812D",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#C7EAE5",
+      "hete_af" = "#F6E8C3",
+      "somatic_af" = "#303030",
+      "gene_name_bg" = "#F5F5F5",
+      "coverage" = "#5AB4AC"
+    )
+  )
+
+  # Color Scheme 7: High contrast safe palette
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FFCC33",
+      "gnomad" = "#003366",
+      "homo_paf" = "#00AA88",
+      "hete_paf" = "#AA3377",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#88CCEE",
+      "hete_af" = "#EE7733",
+      "somatic_af" = "#222222",
+      "gene_name_bg" = "#EAEAEA",
+      "coverage" = "#33BBEE"
+    )
+  )
+
+  # Color Scheme 8: Muted scientific palette
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#E1BE6A",
+      "gnomad" = "#40B0A6",
+      "homo_paf" = "#5AB4AC",
+      "hete_paf" = "#D8B365",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#C7EAE5",
+      "hete_af" = "#F5DEB3",
+      "somatic_af" = "#2B2B2B",
+      "gene_name_bg" = "#F6F6F6",
+      "coverage" = "#01665E"
+    )
+  )
+
+  # Color Scheme 9: Nature journal inspired
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FDC086",
+      "gnomad" = "#386CB0",
+      "homo_paf" = "#7FC97F",
+      "hete_paf" = "#F0027F",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#BEAED4",
+      "hete_af" = "#FDC086",
+      "somatic_af" = "#353535",
+      "gene_name_bg" = "#EDF8E9",
+      "coverage" = "#BF5B17"
+    )
+  )
+
+  # Color Scheme 10: Balanced dichromatic
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#FC8D62",
+      "gnomad" = "#8DA0CB",
+      "homo_paf" = "#66C2A5",
+      "hete_paf" = "#E78AC3",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#A6D854",
+      "hete_af" = "#FFD92F",
+      "somatic_af" = "#292929",
+      "gene_name_bg" = "#E5F5E0",
+      "coverage" = "#E5C494"
+    )
+  )
+
+  # Color Scheme 11: Strong contrast scientific
+  fn_plot_circos(
+    color_circos_track = c(
+      "phastCons100way" = "#EDAD08",
+      "gnomad" = "#1965B0",
+      "homo_paf" = "#7BAFDE",
+      "hete_paf" = "#DC050C",
+      "somatic_paf" = "#000000",
+      "homo_af" = "#B17BA6",
+      "hete_af" = "#F7B6D2",
+      "somatic_af" = "#3C3C3C",
+      "gene_name_bg" = "#F2F2F2",
+      "coverage" = "#4EB265"
+    )
+  )
+
+  dev.off()
+}
 # footer ------------------------------------------------------------------
 
 # save image --------------------------------------------------------------
