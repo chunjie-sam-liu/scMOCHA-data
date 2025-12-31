be827638c5c0325cebf449115fae33590814cb64
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2025-12-31 16:12:09 -0500

    feat: add new R script for Kruskal-Wallis test analysis and data export

---
+++ analysis/high-res/12-variant-celltype-specific.R
@@ -0,0 +1,121 @@
+#!/usr/bin/env Rscript
+# Metainfo ----------------------------------------------------------------
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2025-12-31 15:42:52
+# @DESCRIPTION: this script is used for ...
+
+# Library -----------------------------------------------------------------
+
+load_pkg(jutils)
+
+# args --------------------------------------------------------------------
+
+# s: string, i: integer, f: float, !: boolean, @: array, %: list
+GetoptLong.options(help_style = "two-column")
+VERSION = "v0.0.1"
+
+# default: default value specified here.
+
+verbose = TRUE
+
+GetoptLong("verbose!", "print messages")
+
+
+logger::log_threshold(logger::TRACE)
+logger::log_layout(logger::layout_glue_colors)
+
+# header ------------------------------------------------------------------
+
+# load data ---------------------------------------------------------------
+outdir <- path("/home/liuc9/github/scMOCHA-data/analysis/zzz/MANUSCRIPTFIGURES")
+cleandatadir <- path("/home/liuc9/github/scMOCHA-data/data/zzz/clean-data")
+
+METAFULL <- import(outdir / "SAMPLES-METADATA-FULL.xlsx")
+ALLVARIANTS <- import(
+  outdir / "SAMPLE-VARIANT-CLASSIFICATION-CLUSTER-BULK-AF.xlsx"
+)
+
+# load conn ---------------------------------------------------------------
+
+conn <- DBI::dbConnect(
+  duckdb::duckdb(),
+  "/home/liuc9/github/scMOCHA-data/analysis/zzz/clean-data/all_hetero_af.cell.duckdb.1.2.1",
+  read_only = TRUE
+)
+DBI::dbListTables(conn)
+tbl_allvariants_cell <- dplyr::tbl(
+  conn,
+  "allvariants_cell"
+)
+
+
+# src ---------------------------------------------------------------------
+
+# function ----------------------------------------------------------------
+fn_kruskal_test <- function(.gseid, .srrid, .variant) {
+  # .gseid <- "GSE226602"
+  # .srrid <- "GSM7080053"
+  # .variant <- "11251A>G"
+  log_info(glue::glue("Processing {.gseid} - {.srrid} - {.variant}"))
+  tbl_allvariants_cell |>
+    dplyr::filter(
+      srrid == .srrid,
+      variant == .variant
+    ) |>
+    dplyr::filter(variant_type == "colorful") |>
+    as.data.table() |>
+    dplyr::mutate(
+      af = ifelse(
+        af < 0.01,
+        NA_real_,
+        af
+      )
+    ) |>
+    dplyr::filter(celltype != "other") -> dt_variant_celltype_af
+
+  tryCatch(
+    {
+      kruskal.test(
+        af ~ celltype,
+        data = dt_variant_celltype_af
+      ) |>
+        broom::tidy()
+    },
+    error = \(e) {
+      return(tibble::tibble(
+        statistic = NA_real_,
+        p.value = NA_real_,
+        method = "Kruskal-Wallis test",
+        parameter = NA_real_
+      ))
+    }
+  )
+}
+# body --------------------------------------------------------------------
+
+ALLVARIANTS |>
+  # head(200) |>
+  dplyr::mutate(
+    kruskal_test = parallel::mcmapply(
+      FUN = fn_kruskal_test,
+      .gseid = gseid,
+      .srrid = srrid,
+      .variant = variant,
+      SIMPLIFY = FALSE,
+      mc.cores = 20
+    )
+  ) |>
+  tidyr::unnest(kruskal_test) -> ALLVARIANTS_KRUSKAL
+
+
+{
+  export(
+    ALLVARIANTS_KRUSKAL,
+    outdir /
+      "SAMPLE-VARIANT-KRUSKAL-WALLIS-TEST.xlsx"
+  )
+}
+# footer ------------------------------------------------------------------
+
+# save image --------------------------------------------------------------
