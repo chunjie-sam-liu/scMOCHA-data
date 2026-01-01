e988acdb6317bc2c5a800c6d5f04acca61a2abd5
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2026-01-01 00:10:11 -0500

    feat: add mschart functionality and enhance workbook with new charts and drawings

--- misc/a.R
+++ misc/a.R
@@ -96,6 +96,79 @@ wb$add_worksheet("add_plot")$add_plot(
   units = "in"
 )
 
+tmp <- tempfile(fileext = ".xml")
+rvg::dml_xlsx(file = tmp, fonts = list(sans = "Bradley Hand"))
+print(p)
+dev.off()
+
+# Add rvg to the workbook
+wb$add_worksheet("add_drawing")$add_drawing(xml = tmp)$add_drawing(
+  xml = tmp,
+  dims = NULL
+)
+
+library(mschart) # mschart >= 0.4 for openxlsx2 support
+
+## create chart from mschart object (this creates new input data)
+mylc <- ms_linechart(
+  data = browser_ts,
+  x = "date",
+  y = "freq",
+  group = "browser"
+)
+
+wb$add_worksheet("add_mschart")$add_mschart(dims = "A10:G25", graph = mylc)
+
+
+## create chart referencing worksheet cells as input
+# write data starting at B2
+wb$add_worksheet("add_mschart - wb_data")$add_data(
+  x = mtcars,
+  dims = "B2"
+)$add_data(x = data.frame(name = rownames(mtcars)), dims = "A2")
+
+# create wb_data object this will tell this mschart
+# from this PR to create a file corresponding to openxlsx2
+dat <- wb_data(wb, dims = "A2:G10")
+
+# create a few mscharts
+scatter_plot <- ms_scatterchart(
+  data = dat,
+  x = "mpg",
+  y = c("disp", "hp")
+)
+
+bar_plot <- ms_barchart(
+  data = dat,
+  x = "name",
+  y = c("disp", "hp")
+)
+
+area_plot <- ms_areachart(
+  data = dat,
+  x = "name",
+  y = c("disp", "hp")
+)
+
+line_plot <- ms_linechart(
+  data = dat,
+  x = "name",
+  y = c("disp", "hp"),
+  labels = c("disp", "hp")
+)
+
+# add the charts to the data
+wb$add_mschart(dims = "F4:L20", graph = scatter_plot)$add_mschart(
+  dims = "F21:L37",
+  graph = bar_plot
+)$add_mschart(dims = "M4:S20", graph = area_plot)$add_mschart(
+  dims = "M21:S37",
+  graph = line_plot
+)
+
+# add chartsheet
+wb$add_chartsheet()$add_mschart(graph = scatter_plot)
+
 Sys.setenv(R_ZIPCMD = Sys.which("zip"))
 
 wb$save("mtcars.xlsx")
