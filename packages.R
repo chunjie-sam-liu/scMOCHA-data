0f6a3dcab7d83a569a953dce637fee2db0f14062
chunjie-sam-liu <chunjie.sam.liu@gmail.com>
2026-05-18 23:21:16 -0500

    Add initial project configuration files including Makevars, Rprofile, and Pixi settings

---
+++ packages.R
@@ -0,0 +1,59 @@
+options(
+  repos = c(CRAN = "https://cloud.r-project.org"),
+  install.packages.check.source = "no"
+)
+
+Sys.setenv(
+  R_REMOTES_NO_ERRORS_FROM_WARNINGS = "true",
+  R_REMOTES_UPGRADE = "never"
+)
+remotes::install_github(
+  "RcppCore/RcppParallel",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+
+remotes::install_github(
+  "JanMarvin/openxlsx2",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+
+remotes::install_github(
+  "qsbase/qs2",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+remotes::install_github(
+  "chunjie-sam-liu/jutils",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+remotes::install_github(
+  "satijalab/seurat-data",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+remotes::install_github(
+  "mojaveazure/seurat-disk",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
+
+remotes::install_github(
+  "satijalab/azimuth",
+  upgrade = "never",
+  dependencies = TRUE,
+  force = TRUE
+)
